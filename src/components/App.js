f19b1a356967fd8bdb3aea8c5448b65b9ea40842
jguz17 <johnathanguzman17@gmail.com>
2020-11-17 19:21:37 -0600

    minor tweaks to nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,38 @@
 import '../App.css';
 import { Grid } from '@material-ui/core'
 import Home from '../components/pages/Home'
+import Welcome from '../components/pages/welcome/Welcome'
 import Navbar from '../components/layout/Navbar'
 import UserLocationState from '../context/userLocation/UserLocationState'
+import { BrowserRouter as Router,
+         Switch,
+         Route,
+         Link
+} from 'react-router-dom'
 
 function App() {
   return (
-        <div className="App">
-          <UserLocationState>
-            <Grid container direction='column'>
-              <Grid item xs={12}>
-                <Navbar/>
-              </Grid>
-              <Grid container item direction='row'>
-                <Grid item xs={1}/>
-                <Grid item xs={10}>
-                  <Home/>
+        <Router>
+          <div className="App">
+            <UserLocationState>
+              <Grid container direction='column'>
+                <Grid item xs={12}>
+                  <Navbar/>
                 </Grid>
-                <Grid item xs={1}/>
+                <Grid container item direction='row'>
+                  <Grid item xs={1}/>
+                  <Grid item xs={10}>
+                    <Switch>
+                      <Route path='/home' component={Home}/>
+                      <Route path='/' component={Welcome}/>
+                    </Switch>
+                  </Grid>
+                  <Grid item xs={1}/>
+                </Grid>
               </Grid>
-            </Grid>
-          </UserLocationState>
-        </div>
+            </UserLocationState>
+          </div>
+        </Router>
   );
 }
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,12 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import logo from '../../media/magnetic.png'
 import { Grid } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,7 +38,17 @@ export default function Navbar() {
               <Grid item xs={10}>
                 <div className={classes.navbarWrapper}>
                   <img style={{height: '40px', paddingRight: '1rem'}} src={logo}/> 
-                  <h1 id='page-title' style={{color: '#000'}}>Google Leads</h1>
+                  <h1 id='page-title' style={{color: '#000', flexGrow: '1'}}>Google Leads</h1>
+                  <div>
+                    <ul className='navlinks'>
+                      <li>
+                        <Link to='/home'>Home</Link>
+                      </li>
+                      <li>
+                        <Link to='/'>Welcome</Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </Grid>
               <Grid item xs={1}/>
